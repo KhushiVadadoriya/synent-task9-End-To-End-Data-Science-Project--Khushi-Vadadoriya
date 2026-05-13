0a1ce58f76f736ad4c5abd49f762e98b57891357
Khushivee <vadadoriyakhushi18@gmail.com>
2026-05-13 12:25:54 +0530

    Refactor utils.py for improved object handling and model evaluation; enhance error management

--- src/utils.py
+++ src/utils.py
@@ -1,39 +1,61 @@
 import os
-import pickle
+import sys
 
+import numpy as np 
+import pandas as pd
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
 def save_object(file_path, obj):
-	directory = os.path.dirname(file_path)
-	if directory:
-		os.makedirs(directory, exist_ok=True)
+    try:
+        dir_path = os.path.dirname(file_path)
 
-	with open(file_path, "wb") as file_obj:
-		pickle.dump(obj, file_obj)
+        os.makedirs(dir_path, exist_ok=True)
 
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
 def load_object(file_path):
-	with open(file_path, "rb") as file_obj:
-		return pickle.load(file_obj)
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
-	model_report = {}
-	for model_name, model in models.items():
-		model_params = param.get(model_name, {})
-		if model_params:
-			grid_search = GridSearchCV(model, model_params, cv=3, scoring="r2", n_jobs=-1)
-			grid_search.fit(X_train, y_train)
-			best_model = grid_search.best_estimator_
-			models[model_name] = best_model
-		else:
-			model.fit(X_train, y_train)
-			best_model = model
-			models[model_name] = best_model
-
-		predictions = best_model.predict(X_test)
-		model_report[model_name] = r2_score(y_test, predictions)
-
-	return model_report
+    except Exception as e:
+        raise CustomException(e, sys)
